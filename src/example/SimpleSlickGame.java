d680c11e6812f5497b38732a8ca374df63ed5836
HansEmil <he_hoppe.r@hotmail.com>
2015-03-20 14:36:32 +0100

    another initial change for fun

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -28,7 +28,7 @@ public class SimpleSlickGame extends BasicGame
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		g.drawString("Second change to program", 300, 200);
+		g.drawString("hundreth change to program", 300, 200);
 	}
 
 	public static void main(String[] args)
