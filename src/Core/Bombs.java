4b8ce12655bbeb2d1b1a8419f73c3a0f7c684b3b
nibustan <nan13@student.aau.dk>
2015-03-30 14:10:28 +0200

    trying to create a bomb timer

--- src/Core/Bombs.java
+++ src/Core/Bombs.java
@@ -3,14 +3,14 @@ package Core;
 public class Bombs {
 
 	boolean isExploded = false;
+	int time = 0;
 	
-	
-	public static void main(String[] args) {
-		
-
-	}
-	public void timerBomb(){
-		//COUNTDOWN FOR BOMB EXPLOSION
+	public void timerBomb(GameController gc, int delta){ //COUNTDOWN FOR BOMB EXPLOSION
+		time += delta; //add the time passed since last update()
+	    if(time > 3000){ //3 seconds = 3000 ms
+	    	explodeBomb();
+	    	time = 0;
+	    }	   
 	}
 	
 	public void explodeBomb(){
