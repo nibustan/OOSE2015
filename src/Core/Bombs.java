f3050a080880a2da90e5611d87221fb91162b28d
nibustan <nan13@student.aau.dk>
2015-03-30 13:38:30 +0200

    Made a bomb class

---
+++ src/Core/Bombs.java
@@ -0,0 +1,24 @@
+package Core;
+
+public class Bombs {
+
+	boolean isExploded = false;
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+	public void timerBomb(){
+		//COUNTDOWN FOR BOMB EXPLOSION
+	}
+	
+	public void explodeBomb(){
+		//EXPLOSION FOR BOMB
+	}
+	
+	public void removeBomb(){
+		//REMOVE BOMB AFTER EXPLOSION
+	}
+
+}
